7471a6604c9d5901b343c141ff3f148d82df0fb9
billychorey <billychorey@gmail.com>
2015-11-06 15:04:40 -0500

    11/6

--- zverse_site/js/script.js
+++ zverse_site/js/script.js
@@ -3,56 +3,64 @@
 (function( window, undefined ){
 
 
-	$(".availablebutton").click(function(){
-        $(".onoff").toggleClass("orangefill");
-    });
+	//This is for -- page to do --
+	$( function(){
+
+		$(".availablebutton").click(function(){
+	        $(".onoff").toggleClass("orangefill");
+	    });
+	} );
 
 
 
-	$(".billingbuttons").click(function(){
-        $(".payselect").toggleClass("graybackground");
-    });
+	//this is to toggle the billing button page
+	$( function(){
+
+		var billingbuttons = $( ".billingbuttons a" );
+		var payselect      = $( ".payselect" );
+		var pages          = $( ".page" );
+		
+		billingbuttons.on( "click", function( e ){
+
+			var atag      = $( this );
+			var classname = atag.data( "page" );
+
+			//changes the tab colors from gray to white when clicked
+	        payselect.addClass( "graybackground" );
+	        atag.parent().removeClass( "graybackground" );
+
+	        //hide the pages
+			pages.hide();
+			//show the page related to the button clicked
+			$( classname ).show();
+
+			//prevents the page from reloading
+	        e.preventDefault();
+	    });
+
+	    $( ".echeckbutton a" ).trigger( "click" );
+	} );
 
 
 
-    var billingbuttons = $(".billingbuttons a")
+	// var detailsButton = $(".moreinfo")
 
-	var divName = $(".options div")
+	// var projectInfo = $(".additionalinformation")
 
-	billingbuttons.on("click", function( e ){
+	// detailsButton.on("click", function( e ){
 
-	var className = $( this ).data( "page");
+	// var className = $( this ).data( "page");
 
-	divName.hide();
-	$( className ).fadeIn( 200 );
-
-	$( className ).show();
-
-	e.preventDefault();
-	});
-
-	divName.hide();
-
-
-
-	var detailsButton = $(".moreinfo")
-
-	var projectInfo = $(".additionalinformation")
-
-	detailsButton.on("click", function( e ){
-
-	var className = $( this ).data( "page");
-
-	projectInfo.hide();
+	// projectInfo.hide();
 	
-	$( projectInfo ).fadeIn( 200 );
+	// $( projectInfo ).fadeIn( 200 );
 
-	$( className ).show();
+	// $( className ).show();
 
-	e.preventDefault();
-	});
+	// e.preventDefault();
+	// });
 
-	divName.hide();
+	// divName.hide();
 	
 
 
